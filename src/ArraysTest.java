422ae3b2aecb82036b8e6ef70a9c417894e4a378
oldling <oldling@gmail.com>
2022-06-24 00:53:13 +0300

    findCommon done

--- src/ArraysTest.java
+++ src/ArraysTest.java
@@ -13,17 +13,26 @@ public class ArraysTest {
     @DataProvider(name = "ArrayUtil")
     public static Object[][] arrays() {
         return new Object[][]{
-                {
-                        new String[]{"Python", "JAVA", "PHP", "C#", "C++", "SQL"},
-                        new String[]{"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"},
-                        new String[]{"JAVA", "SQL"}
-                },
-                {
-                        new String[]{"here", "is", "some", "text", ".", "text"},
-                        new String[]{"some", "text", "above", "."},
-                        new String[]{"some", "text", "."}
-                }
-                // TODO add 2 more test data here
+            {
+                new String[]{"Python", "JAVA", "PHP", "C#", "C++", "SQL"},
+                new String[]{"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"},
+                new String[]{"JAVA", "SQL"}
+            },
+            {
+                new String[]{"here", "is", "some", "text", ".", "text"},
+                new String[]{"some", "text", "above", "."},
+                new String[]{"some", "text", "."}
+            },
+            {
+                new String[]{"1", "2", "9", "4", "5", "6", "7", "8", "9"},
+                new String[]{"5", "8", "7", "6", "9", "4"},
+                new String[]{"4", "5", "6", "7", "8", "9"}
+            },
+            {
+                new String[]{"all same", "all same", "all same", "all same", "all same", "all same", "all same"},
+                new String[]{"all same", "all same", "all same", "all same", "all same", "all same", "all same"},
+                new String[]{"all same"}
+            }
         };
     }
 
